959d25122f14d1799987f3adbd02ccd26758bc83
Justin Cook <jcfarsight@gmail.com>
2026-06-01 15:47:33 +1200

    Switch punctuation

--- src/app/custom/page.tsx
+++ src/app/custom/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="md:text-lg text-sm">
         Here are some of the custom orders that I&apos;ve had the privilege to create 🤍
         <br />
-        Please enquire if you require something custom.
+        Please enquire if you require something custom!
       </div>
       <Image src="/custom-order-collage.jpg" className="ml-auto mr-auto mt-5 mb-5 w-[320px] md:w-[800px]" width="0" height="0" alt="Custom Orders" />
     </div>
